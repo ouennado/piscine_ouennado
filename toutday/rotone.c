5bbd791d4c13288cc490661d62da7d81fa72db94
ouennado <110168252+ouennado@users.noreply.github.com>
2022-07-28 16:18:56 +0100

    Add files via upload

--- toutday/rotone.c
+++ toutday/rotone.c
@@ -10,7 +10,8 @@ int		main(int ac, char **av)
 	{
 		while (av[1][i])
 		{
-			if ((av[1][i] >= 'A' && av[1][i] <= 'Y') || (av[1][i] >= 'a' && av[1][i] <= 'y'))
+			if ((av[1][i] >= 'A' && av[1][i] <= 'Y')
+				|| (av[1][i] >= 'a' && av[1][i] <= 'y'))
 			{
 				c = av[1][i] + 1;
 				write(1, &c, 1);
