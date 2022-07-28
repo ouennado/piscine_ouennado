cdf1abb3ef9f52ce88c61fd00f063b3a973932d9
ouennado <110168252+ouennado@users.noreply.github.com>
2022-07-28 13:04:26 +0100

    Add files via upload

---
+++ toutday/c00/ex06/ft_print_comb2.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ouennado <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/23 19:49:45 by ouennado          #+#    #+#             */
+/*   Updated: 2022/07/24 00:17:53 by ouennado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = -1;
+	while (a++ < 98)
+	{
+		b = a;
+		while (b++ < 99)
+		{
+			ft_putchar((a / 10) + '0');
+			ft_putchar((a % 10) + '0');
+			ft_putchar(' ');
+			ft_putchar((b / 10) + '0');
+			ft_putchar((b % 10) + '0');
+			if (a != 98)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+		}
+	}
+}
